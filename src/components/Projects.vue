df00ef8ca6bdc628db11cbcd832c00f6a464b84c
AankitP <2001aankit@gmail.com>
2023-01-27 08:37:06 -0600

    changes
    -Working on updating and formatting projects page
    -this update will not be merged with main until we are closer to finishing the projects and make the CABStone repo public

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -23,13 +23,14 @@
         </ProjHolder>
         <ProjHolder>
             <div class="allHolderProj">
-                <img src="./../assets/VueSiteCode.png"  class="ProjImg"/>
+                <img src="./../assets/MUILogo.png"  class="TechImg"/>
+                <img src="./../assets/REACT_Logo.png"  class="TechImg"/>
                 <div class="textHolderProj">
                     <h3>Current Project: <a href="https://github.com/AankitP/AankitSiteVue" target="_blank">CABStone</a></h3>
                     <h5>
                     &emsp;This project is my capstone, I am working alongside a group of 4 other people to create a 
                     modular DNS application, I am working alongside one of my other groupmates to create the front end
-                    portion of the project. We are using REACT with MUI to create the front end, and GO to create the 
+                    portion of the project. We are using REACT with MUI to create the front end, and RUST to create the 
                     backend and modules.
                     </h5>
                 </div>
@@ -38,13 +39,9 @@
         <ProjHolder>
             <div class="allHolderProj">
                 <div class="textHolderProj">
-                    <h3>Current Project: <a href="https://github.com/AankitP/AankitSiteVue" target="_blank">Website Refactor</a></h3>
+                    <h3>Current Project: <a href="https://github.com/krishabbashyal/electric-edge" target="_blank">Electric Edge</a></h3>
                     <h5>
-                    &emsp;Originally this site was written in strictly HTML and CSS (to be honest it didn't look the best).
-                    At the time I did not have the knowledge to know how to make it look and function better. 
-                    My schedule had started to become more hectic with classes, homework, and exams, so I had to put
-                    this project on the backburner until I got the time to fix it. One day I was helping my cousin with
-                    a site he was working on, and was introduced to vue, so I decided that I would fix my website.
+                    &emsp;This is a project that I am helping my cousin with, The idea is a Electric charger service 
                     </h5>
                 </div>
                 <img src="./../assets/VueSiteCode.png"  class="ProjImg"/>
@@ -93,5 +90,10 @@ export default {
 .ProjImg{
   height: 40%;
   width: 60%;
+}
+
+.TechImg{
+  height: 10rem;
+  width: auto;
 }
 </style>
